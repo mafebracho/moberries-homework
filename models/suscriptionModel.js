d237c0e2271865eb90ee5c91e9262ca797b3ebd7
Mafe Bracho <mafe.bracho@gmail.com>
2021-06-10 12:00:21 +0200

    change model attribute names for suscription model

--- models/suscriptionModel.js
+++ models/suscriptionModel.js
@@ -2,8 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const suscriptionSchema = new Schema({
   duration_months: { type: Number, required: true, default: 12 },
-  size: { type: Number, required: true, default: 5 },
-  upfrontPayment: { type: Boolean, required: true, default: false },
+  size_gb: { type: Number, required: true, default: 5 },
+  upfront_payment: { type: Boolean, required: true, default: false },
   });
   
   const Suscription = model("Suscription", suscriptionSchema);
