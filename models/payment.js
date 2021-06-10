648833a59cb6807e09284699cba0fff3f1ff3dd8
Mafe Bracho <mafe.bracho@gmail.com>
2021-06-10 12:08:08 +0200

    add attributes to payment model

--- models/payment.js
+++ models/payment.js
@@ -1,7 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 const paymentSchema = new Schema({
-  
+  card_number: { type: String, required: true },
+  expiration_date: { type: Date, required: true },
+  security_code: { type: String, required: true }
 });
 
 const Payment = model("Payment", paymentSchema);
