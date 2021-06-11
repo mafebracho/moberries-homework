87b6ad370e062dac9c745f5ad653adc92f25cb6e
Mafe Bracho <mafe.bracho@gmail.com>
2021-06-11 12:47:04 +0200

    add constructor and set default values to the state. listeners logic to change state. start changes in return and start basic styling

--- client/src/components/Subscription.js
+++ client/src/components/Subscription.js
@@ -3,21 +3,43 @@ import axios from 'axios';
 
 class Subscription extends Component {
 
-    state = {
-        subscriptionPlans: []
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedDuration: 12,
+            selectedStorage: 5,
+            isUpfront: false
+        }
+
+        this.changeDuration = this.changeDuration.bind(this)
+        this.changeStorage = this.changeStorage.bind(this)
+        this.changeUpfront = this.changeUpfront.bind(this)
     }
 
     componentDidMount() {
+        // get subscription plan
         axios
         .get('https://cloud-storage-prices-moberries.herokuapp.com/prices')
         .then(res => {
             console.log(res)
-            const subscriptionPlans = res.data.subscription_plans;
-            this.setState({subscriptionPlans: subscriptionPlans})
+            this.setState({ subscriptionPlans: res.data.subscription_plans })
         })
     }
 
+    changeDuration(event) {
+        this.setState({ selectedDuration: parseInt(event.target.value) })
+    }
+
+    changeStorage(event) {
+        this.setState({ selectedStorage: parseInt(event.target.value) })
+    }
+
+    changeUpfront(event) {
+        this.setState({ isUpfront: event.target.value === 'true' })
+    }
+
     render() {
+        if (!this.state || !this.state.subscriptionPlans) return <div/>
         return (
             <div>
             <h1>Upgrade to a plan that works for you</h1>
